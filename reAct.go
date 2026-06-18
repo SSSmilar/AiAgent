c66d99f42a085d6b78d4a7beeb2a6442c7ca44fd
Andrey <realdeadinst@gmail.com>
2026-06-18 14:00:41 +0300

    feat: enhance ReAct function to support tool usage and contributor retrieval

--- reAct.go
+++ reAct.go
@@ -21,30 +21,63 @@ Action: FINAL: <ответ, в котором ты уверен>
 
 const Observation = "Observation: перечитай свой вариант. Есть ошибка - исправь, иначе зафиксируй FINAL."
 
-func ReAct(apiKey string, task string) {
-	dialogs := []Message{{
-		Role:    "user",
-		Content: task,
-	}}
+func ReAct(apiKey string, Tools []Tool, task string) {
+	var dialogs []Message
+
+	dialogs = append(dialogs, Message{Role: "user", Content: task})
+
 	//Ограничил ReAct  5 запросами так как при долгом размышлении она может просто дизентигрировать токены .
 	for i := 0; i < 5; i++ {
 		//Вызываю запрос к LLM
-		response, err := ask(apiKey, reactSystem, dialogs)
+		respMsg, err := ask(apiKey, Tools, reactSystem, dialogs)
 		if err != nil {
 			slog.Error("Error sending request to API ", "details", err)
 			os.Exit(1)
 		}
-		//Вывожу  в консоль на каждом этапе чтобы видеть процесс размышления .
-		fmt.Printf("\n=== Iteration %d ===\n ", i+1)
-		fmt.Println(response)
-		fmt.Println("=== End ===\n ")
-		//Вывожу ответы в консоль чтобы видеть как модель думает .
-		dialogs = append(dialogs, Message{Role: "assistant", Content: response})
-		//Проверяю на финал , если не финал отправляю модель дальше думать .
-		if strings.Contains(response, "FINAL") {
-			break
-		}
+		//Оригинальный ответ модели добавляю 1 раз .
+		dialogs = append(dialogs, respMsg)
 
+		//Ветка с работы модели без вызова инструментов .
+		if len(respMsg.ToolCalls) == 0 {
+			//Вывожу  в консоль на каждом этапе чтобы видеть процесс размышления .
+			fmt.Printf("\n=== Iteration %d ===\n ", i+1)
+			fmt.Println(respMsg.Content)
+			fmt.Println("=== End ===\n ")
+			//Вывожу ответы в консоль чтобы видеть как модель думает .
+			dialogs = append(dialogs, Message{Role: "assistant", Content: respMsg.Content})
+			//Проверяю на финал , если не финал отправляю модель дальше думать .
+			if strings.Contains(respMsg.Content, "FINAL") {
+				break
+			}
+		}
+		
+		dialogs = append(dialogs, respMsg)
+		for _, i := range respMsg.ToolCalls {
+			if i.Function.Name == "GetContributors" {
+				contributirs, err := GetContributors()
+				if err != nil {
+					slog.Error("Error receiving contributors ", "details", err)
+					errors, err := json.Marshal(err)
+					if err != nil {
+						slog.Error("Error converting error to json ", "details", err)
+					}
+					dialogs = append(dialogs, Message{Role: "assistant", Content: string(errors)})
+					continue
+				}
+				contributirsJSON, err := json.Marshal(contributirs)
+				if err != nil {
+					slog.Error("Error converting contributirs to json ", "details", err)
+					errors, err := json.Marshal(err)
+					if err != nil {
+						slog.Error("Error converting error to json ", "details", err)
+					}
+					dialogs = append(dialogs, Message{Role: "assistant", Content: string(errors)})
+					continue
+				}
+				dialogs = append(dialogs, Message{Role: "tool", Content: string(contributirsJSON), ToolCallID: i.ID})
+				continue
+			}
+		}
 		dialogs = append(dialogs, Message{Role: "user", Content: Observation})
 	}
 }
